9d84718369c80372d9f4d9c6b876f57b0d4bac83
Zelin Chen <6692czl@gmail.com>
2022-10-09 22:09:13 -0700

    A67(1)

--- A67.hpp
+++ A67.hpp
@@ -4,6 +4,46 @@
 #include <iostream>
 using namespace std;
 
+void getinput(int &num)
+{
+    cin>>num;
+}
+void getinput(int &val1,int &val2)
+{
+    cin>>val1>>val2;
+}
+void getinput(int &val1,int &val2,int &val3)
+{
+    cin>>val1>>val2>>val3;
+}
+void getinput(int &val1,int &val2,int val3,int &val4)
+{
+    cin>>val1>>val2>>val3>>val4;
+}
+void swap(int &val1,int &val2)
+{
+    int temp;
+    temp=val1;
+    val1=val2;
+    val2=temp;
+}
+void swap(int &val1,int &val2,int &val3)
+{
+    int temp;
+    temp=val1;
+    val1=val3;
+    val3=val2;
+    val2=temp;
+}
+void swap(int &val1,int &val2,int &val3,int &val4)
+{
+    int temp;
+    temp=val1;
+    val1=val4;
+    val4=val3;
+    val3=val2;
+    val2=temp;
+}
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
